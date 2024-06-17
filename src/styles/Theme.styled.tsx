7afdacd845b12614243d8576df9dd568694e3a60
KaminnikAndrey <andreytennis66@gmail.com>
2024-06-18 01:48:22 +0500

    8

---
+++ src/styles/Theme.styled.tsx
@@ -0,0 +1,12 @@
+export const myTheme = {
+    colors: {
+        dark: "rgba(40, 44, 51, 1)",
+        grey: "rgba(171, 178, 191, 1)",
+        white: "rgba(255, 255, 255, 1)",
+        purple: "rgba(199, 120, 221, 1)"
+    },
+    media: {
+        tablet: "screen and (max-width:768px)",
+        mobile: "screen and (max-width:576px)",
+    }
+}
