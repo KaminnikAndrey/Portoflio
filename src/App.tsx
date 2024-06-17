41d737c244db69c88b07c5afcf1a9a0014d4f05b
KaminnikAndrey <andreytennis66@gmail.com>
2024-06-17 20:20:27 +0500

    11

---
+++ src/App.tsx
@@ -0,0 +1,19 @@
+import './App.css';
+import styled from 'styled-components';
+
+
+function App() {
+    return (
+        <div className="App">
+            <Title>Welcome to IT-INCUBATOR</Title>
+        </div>
+    );
+}
+
+export default App;
+// 13213
+const Title = styled.h1`
+    font-size: 1.5em;
+    text-align: center;
+    color: #e91e63;
+`;
